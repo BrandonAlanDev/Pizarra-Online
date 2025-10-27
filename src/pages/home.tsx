5e289ee54ba1f42c3bb16e9c90fca3d28afe8d7b
BrandonAlanDev <brandoncarabajal@gmail.com>
2025-10-26 23:56:07 -0300

    fix not usages variables and imports

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import Pizarra from '../components/Pizarra'
 import '../App.css'
 
 function Home() {
-  const [count, setCount] = useState(0)
 
   return (
         <div className="flex flex-col justify-center items-center bg-linear-60 from-white to-[#bebfff] w-[100vw] h-[100vh] text-black">
